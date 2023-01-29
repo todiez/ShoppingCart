2717cc89e36871e567116824595dd2b7390d02a6
tybo-dev <tobias@diez.guru>
2023-01-29 12:06:35 +0100

    testing tests

--- shopping-cart/src/App.test.js
+++ shopping-cart/src/App.test.js
@@ -1,8 +1,28 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { api } from './api';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const mockCreateItem = (api.createItem = jest.fn());
+
+
+test('add item to list', async () => {
+  const cartText = "Apples";
+  mockCreateItem.mockResolvedValueOnce(cartText);
+
+  const {getByText, getByLabelText} = render(<App></App>);
+
+  //enter content, interact with page
+  const input = getByLabelText("Cart Contents");
+  fireEvent.change(input, {target:{value:"Apples"}});
+  fireEvent.click(getByText("$3 Apples - Stock: 10"));
+
+  await wait(() => getByText("Apples"));
+
+  //confirm data
+  expect(mockCreateItem).toBeCalledTimes(1);
+  expect(mockCreateItem).toBeCalledWith(
+    expect.stringContaining("Apples")
+  );
+
+
 });
